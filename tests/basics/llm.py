99a7a30bfbeba1723441e92af143cc8535843cd7
Madhav Kandukuri <madhav165@gmail.com>
2024-08-05 19:59:42 +0530

    testing

--- tests/basics/llm.py
+++ tests/basics/llm.py
@@ -15,19 +15,19 @@ openai_llm = ChatOpenAI(
 #HuggingFace
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
-llm = HuggingFaceEndpoint(
-    repo_id="google/gemma-2-27b-it",
+huggingface_llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
     task="text-generation",
     max_new_tokens=512,
     do_sample=False,
     repetition_penalty=1.03,
 )
-
-huggingface_llm = ChatHuggingFace(llm=llm)
+huggingface_llm = ChatHuggingFace(llm=huggingface_llm)
 
 #NVIDIA
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 # nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-8b-instruct")
-nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-70b-instruct")
+# nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-70b-instruct")
 # nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-405b-instruct")
+nvidia_llm = ChatNVIDIA(model="google/gemma-2-27b-it")
